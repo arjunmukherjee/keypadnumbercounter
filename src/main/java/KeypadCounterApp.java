12b8917db81e3cea809daa75f96a2b1f19b65055
arjun.mukherjee <arjun.mukherjee@intersection.com>
2017-11-13 00:40:45 -0500

    added a main point

---
+++ src/main/java/KeypadCounterApp.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class KeypadCounterApp {
+
+    public static void main(String[] args) {
+        Point startingPoint = new Point(0,0);
+        KeypadNumberCounter knc = new KeypadNumberCounter(startingPoint, new Keypad(KeyPadTypes.NUMERIC_KEY_PAD), new Diagonal());
+        System.out.println("Number of unique 10 digit numbers is " + knc.getCount(10));
+
+        // TODO : Memoize : This is timing out
+        // System.out.println("Number of unique 20 digit numbers is " + knc.getCount(20));
+    }
+}
